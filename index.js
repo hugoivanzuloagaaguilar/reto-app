4c93a1300b0ea98f6bf02ec3416dfafee1d4116e
hugoivanzuloagaaguilar <81063697+hugoivanzuloagaaguilar@users.noreply.github.com>
2023-02-26 14:52:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World amigos'))
+app.get('/', (req, res) => res.send('Hello World amigos v2'))
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
